31a7e463746c2a7e003912f41a075ab5dac55d6b
WithuL <1509586319@qq.com>
2020-04-27 23:08:03 +0800

    二叉树习题

---
+++ src/Java_04_27/LeetCode.java
@@ -0,0 +1,17 @@
+package Java_04_27;
+
+public class LeetCode {
+    class Solution1 {
+        public int widthOfBinaryTree(TreeNode root) {
+            return Math.max(1,2);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
