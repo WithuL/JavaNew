a9ada6d58fc18abc300ee511e4d2c5f7235c05e1
WithuL <1509586319@qq.com>
2020-05-02 17:44:43 +0800

    平衡二叉树 礼物的最大价值

--- src/Java_0502/LeetCode.java
+++ src/Java_0502/LeetCode.java
@@ -54,5 +54,49 @@ public class LeetCode {
             }
         }
     }
+    //礼物的最大价值
+    class Solution3 {
+        public int maxValue(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            for(int i = 0; i < m; i++ ) {
+                for(int j = 0; j < n; j++) {
+                    if(i == 0 && j == 0) {
+                        continue;
+                    }else if(i == 0) {
+                        grid[i][j] += grid[i][j-1];
+                    }else if(j == 0) {
+                        grid[i][j] += grid[i-1][j];
+                    }else{
+                        grid[i][j] += Math.max(grid[i-1][j],grid[i][j-1]);
+                    }
+                }
+            }
+            return grid[m-1][n-1];
+        }
+    }
+
+    //平衡二叉树
+    class Solution4 {
+        public boolean isBalanced(TreeNode root) {
+            if(root == null) {
+                return true;
+            }
+            if(root.left == null && root.right == null) {
+                return true;
+            }
+            int max1 = max(root.left);
+            int max2 = max(root.right);
+            return isBalanced(root.left) && isBalanced(root.right) && max1 - max2 >= -1 && max1 - max2 <= 1;
+        }
+        public int max(TreeNode root) {
+            if(root == null) {
+                return 0;
+            }
+            int max1 = max(root.left);
+            int max2 = max(root.right);
+            return  1 + Math.max(max1,max2);
+        }
+    }
 
 }
---
+++ src/Java_0502/TreeNode.java
@@ -0,0 +1,10 @@
+package Java_0502;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
