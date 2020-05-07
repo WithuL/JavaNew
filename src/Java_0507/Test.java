cb2c41354ae8b207e762e7438a11be3137d0cfd4
WithuL <1509586319@qq.com>
2020-05-07 17:53:48 +0800

    翻转单词顺序  旋转字符串左侧  从前序与中序遍历序列构造二叉树

--- src/Java_0507/Test.java
+++ src/Java_0507/Test.java
@@ -97,5 +97,44 @@ public class Test {
                 return res.toString().trim();
             }
         }
+        //旋转字符串左侧
+        class Solution {
+            public String reverseLeftWords(String s, int n) {
+                StringBuilder res = new StringBuilder();
+                res.append(s.substring(n,s.length()));
+                return res.append(s.substring(0,n)).toString();
+            }
+        }
+
+        //从前序与中序遍历序列构造二叉树
+        class Solution5 {
+            private int index = 0;
+            public TreeNode buildTree(int[] preorder, int[] inorder) {
+                index = 0;
+                return buildTreeHelper(preorder,inorder,0,preorder.length);
+            }
+            private TreeNode buildTreeHelper(int[] preorder,int[] inorder,int inorderLeft,int inorderRight){
+                if(inorderLeft >= inorderRight) {
+                    return null;
+                }
+                if(index >= preorder.length) {
+                    return null;
+                }
+                TreeNode cur = new TreeNode(preorder[index]);
+                index++;
+                int pos = find(inorder,inorderLeft,inorderRight,cur.val);
+                cur.left = buildTreeHelper(preorder,inorder,inorderLeft,pos);
+                cur.right = buildTreeHelper(preorder,inorder,pos+1,inorderRight);
+                return cur;
+            }
+            public int find(int[] inorder,int inorderLeft,int inorderRight,int val) {
+                for(int i = inorderLeft ; i < inorderRight; i++) {
+                    if(inorder[i] == val) {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
     }
 }
---
+++ src/Java_0507/TreeNode.java
@@ -0,0 +1,8 @@
+package Java_0507;
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
