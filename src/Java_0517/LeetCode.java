ed88b17f7e807a05d6ab1b70423727a9576ffc3b
WithuL <1509586319@qq.com>
2020-05-17 17:26:16 +0800

    合并K个链表  两种解法

--- src/Java_0517/LeetCode.java
+++ src/Java_0517/LeetCode.java
@@ -64,4 +64,64 @@ public class LeetCode {
         }
 
     }
+//  合并k个排序链表
+    class Solution3 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            if(lists.length == 0) {
+                return null;
+            }
+            ListNode res = null;
+            for(ListNode cur : lists) {
+                res = merge2Lists(res,cur);
+            }
+            return res;
+        }
+        private ListNode merge2Lists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            if (l1.val < l2.val) {
+                l1.next = merge2Lists(l1.next, l2);
+                return l1;
+            }
+            l2.next = merge2Lists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    //解法二
+    class Solution4 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            if(lists.length == 0) {
+                return null;
+            }
+            return merge(lists,0,lists.length-1);
+        }
+        public ListNode merge(ListNode[] lists, int left, int right) {
+            if(right == left) {
+                return lists[left];
+            }
+            int mid = left + (right - left) / 2;
+            ListNode leftNode = merge(lists, left, mid);
+            ListNode rightNode = merge(lists, mid + 1 , right);
+            return mergeHelper(leftNode,rightNode);
+        }
+        public ListNode mergeHelper(ListNode left, ListNode right) {
+            if(left == null) {
+                return right;
+            }
+            if(right == null) {
+                return left;
+            }
+            if(left.val < right.val) {
+                left.next = mergeHelper(left.next,right);
+                return left;
+            }
+            right.next = mergeHelper(left,right.next);
+            return right;
+        }
+    }
 }
