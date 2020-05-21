02ddda10546e133631608df45d8c668574f350ee
WithuL <1509586319@qq.com>
2020-05-21 18:18:46 +0800

    旋转图像  方法不太好

--- src/Java_0521/LeetCode.java
+++ src/Java_0521/LeetCode.java
@@ -32,4 +32,30 @@ public class LeetCode {
             }
         }
     }
+    //旋转图像 这个方法不太好
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int rowStart = 0;
+            int colStart = 0;
+            int rowEnd = matrix.length - 1;
+            int colEnd = matrix[0].length - 1;
+            while(rowStart < rowEnd){
+                helper(matrix,rowStart,colStart,rowEnd,colEnd);
+                rowStart++;
+                colStart++;
+                rowEnd--;
+                colEnd--;
+            }
+        }
+        public void helper(int[][] matrix, int rowStart, int colStart, int rowEnd, int colEnd){
+            int time = rowEnd - rowStart;
+            for(int i = 0; i < time ; i++) {
+                int tmp = matrix[rowStart][colStart+i];
+                matrix[rowStart][colStart+i] = matrix[rowEnd - i][colStart];
+                matrix[rowEnd - i][colStart] = matrix[rowEnd][colEnd - i];
+                matrix[rowEnd][colEnd - i] = matrix[rowStart + i][colEnd];
+                matrix[rowStart + i][colEnd] = tmp;
+            }
+        }
+    }
 }
