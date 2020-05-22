a40b82f17eb50829ced582663bb60560fb8c75fb
WithuL <1509586319@qq.com>
2020-05-22 17:26:43 +0800

    最小路径和 和 不同路径

--- src/Java_0522/LeetCode.java
+++ src/Java_0522/LeetCode.java
@@ -48,4 +48,68 @@ public class LeetCode {
             return Arrays.copyOf(res,ret.size());
         }
     }
+    //不同路径
+
+    //递归解法：超时
+    class Solution3 {
+        public int uniquePaths(int m, int n) {
+            int row = 1;
+            int col = 1;
+            return helper(row,col,m,n);
+        }
+        public int helper(int row, int col ,int m, int n) {
+            if(row == m && col == n) {
+                return 1;
+            }
+            int res = 0;
+            if(row < m) {
+                res += helper(row + 1,col,m,n);
+            }
+            if(col < n) {
+                res += helper(row ,col +1, m , n);
+            }
+            return res;
+        }
+    }
+
+    //动态规划
+    class Solution4 {
+        public int uniquePaths(int m, int n) {
+            //这个数组用来存储到达res[i][j]位置有多少种方法
+            int[][] res= new int[m][n];
+            for(int i = 0; i < m; i++) {
+                res[i][0] = 1;
+            }
+            for(int i = 0; i < n; i++) {
+                res[0][i] = 1;
+            }
+            //去除掉最外层开始计算
+            for(int i = 1 ; i < m; i++) {
+                for(int j = 1; j < n; j++) {
+                    res[i][j] = res[i-1][j] + res[i][j-1];
+                }
+            }
+            return res[m-1][n-1];
+        }
+    }
+
+    //最小路径和
+    class Solution5 {
+        public int minPathSum(int[][] grid) {
+            for(int i = 0; i < grid.length; i++){
+                for(int j = 0;j < grid[0].length; j++){
+                    if(i ==0 && j == 0) {
+                        continue;
+                    }else if(i == 0 ){
+                        grid[i][j] += grid[i][j-1];
+                    }else if(j == 0) {
+                        grid[i][j] += grid[i-1][j];
+                    }else {
+                        grid[i][j] += Math.min(grid[i][j-1], grid[i-1][j]);
+                    }
+                }
+            }
+            return grid[grid.length-1][grid[0].length-1];
+        }
+    }
 }
