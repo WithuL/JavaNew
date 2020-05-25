a004808cf95ff18ddce447655b12a07e427ca0d1
WithuL <1509586319@qq.com>
2020-05-25 16:27:38 +0800

    数组中的第K个最大元素

--- src/Java_0525/LeetCode.java
+++ src/Java_0525/LeetCode.java
@@ -37,4 +37,12 @@ public class LeetCode {
             return res.next;
         }
     }
+	
+	//数组中的第K个最大元素
+	class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+}
 }
