d08c8a56247238de0d23925bd6f2848e78474e9e
WithuL <1509586319@qq.com>
2020-06-02 16:11:36 +0800

    每日一题练习

--- src/Java_0601/Main.java
+++ src/Java_0601/Main.java
@@ -87,35 +87,35 @@ public class Main {
 
     public class FindKthNum {
 
-        private  int findKthNum(int[] num, int len, int k) {
-            return quickSort(num, 0, len-1, k);
+        private int findKthNum(int[] num, int len, int k) {
+            return quickSort(num, 0, len - 1, k);
         }
 
-        private  int quickSort(int[] num, int low, int high, int k){
-            if(low <= high){
+        private int quickSort(int[] num, int low, int high, int k) {
+            if (low <= high) {
                 int pos = partition(num, low, high);
-                if(pos == k-1)
+                if (pos == k - 1)
                     return num[pos];
-                else if(pos > k-1)
-                    return quickSort(num, low, pos-1, k);
+                else if (pos > k - 1)
+                    return quickSort(num, low, pos - 1, k);
                 else
-                    return quickSort(num, pos+1, high, k);
-            }else
+                    return quickSort(num, pos + 1, high, k);
+            } else
                 return -1;
         }
 
-        private  int partition(int[] num, int low, int high) {
+        private int partition(int[] num, int low, int high) {
             int tmp = num[low];
-            while(low < high){
-                while((low < high) && tmp >= num[high])//（1）
+            while (low < high) {
+                while ((low < high) && tmp >= num[high])//（1）
                     high--;
                 num[low] = num[high];
-                while((low < high) && tmp <= num[low])//（2）
+                while ((low < high) && tmp <= num[low])//（2）
                     low++;
                 num[high] = num[low];
             }
             num[low] = tmp;
             return low;
         }
-
+    }
 }
---
+++ src/Java_0602/LeetCode.java
@@ -0,0 +1,45 @@
+package Java_0602;
+
+import java.util.Scanner;
+
+public class LeetCode {
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4,5,6,7,0};
+        System.out.println(AntiOrder.count(A,8));
+    }
+}
+class AntiOrder {
+    public static int count(int[] A, int n) {
+        // write code here
+        int count = 0;
+        for(int end = n-1; end > 0; end--) {
+            for(int pre = end - 1; pre >= 0; pre--) {
+                if(A[pre] > A[end]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+ class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int ds = sc.nextInt();
+            int count = 0;
+            int use = 0,left = 0;
+            while(ds > 1) {
+                if(use + left == 2) {
+                    count += 1;
+                    break;
+                }
+                use = ds / 3;
+                left = ds % 3;
+                count += use;
+                ds = use + left;
+            }
+            System.out.println(count);
+        }
+    }
+}
