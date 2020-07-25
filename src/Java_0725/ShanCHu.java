4bca932a588c8d05df5a8b072478aed49da2d4a4
WithuL <1509586319@qq.com>
2020-07-25 11:29:58 +0800

    上传

---
+++ src/Java_0725/ShanCHu.java
@@ -0,0 +1,26 @@
+package Java_0725;
+
+import java.util.Scanner;
+
+public class ShanCHu {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in) ;
+//        while(sc.hasNextLine()) {
+//            String l1 = sc.nextLine();
+//            String l2 = sc.nextLine();
+//            for(int i = 0; i < l1.length(); i++) {
+//                char tmp = l1.charAt(i);
+//                if(!l2.contains(tmp+"")) {
+//                    System.out.print(tmp);
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+    public static void main(String[] args) {
+        int len = 'z';
+        boolean[] arr = new boolean[len];
+        
+        System.out.println();
+    }
+}
