99a666230230531329d2e60bb40c555a17c5d611
WithuL <1509586319@qq.com>
2020-07-25 11:30:36 +0800

    数字分类

---
+++ src/Java_0707/ShuziFenlei.java
@@ -0,0 +1,14 @@
+package Java_0707;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ShuziFenlei {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()) {
+
+        }
+    }
+}
