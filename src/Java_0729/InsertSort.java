be06b3f42e865efab82ffbd26824b78ca5cb0b73
WithuL <1509586319@qq.com>
2020-07-29 15:58:11 +0800

    插入排序

--- src/Java_0729/InsertSort.java
+++ src/Java_0729/InsertSort.java
@@ -1,5 +1,18 @@
 package Java_0729;
 
 public class InsertSort {
-
+    public void inserSort(int[] arr) {
+        for(int bound = 1; bound < arr.length; bound++) {
+            int cur = bound - 1;
+            int tmp = arr[bound];
+            for(; cur >= 0; cur--) {
+                if(arr[cur] > tmp) {
+                    arr[cur+1] = arr[cur];
+                }else {
+                    break;
+                }
+            }
+            arr[cur+1] = tmp;
+        }
+    }
 }
