e1a04926b79e2f0789eb9e4612cf7c1143d40e06
WithuL <1509586319@qq.com>
2020-08-04 09:17:42 +0800

    插入排序

--- src/Java_0804/InsertSort.java
+++ src/Java_0804/InsertSort.java
@@ -1,12 +1,12 @@
-package Java_0729;
+package Java_0804;
 
 public class InsertSort {
     public void insertSort(int[] arr) {
-        for(int bound = 1; bound < arr.length; bound++) {
-            int cur = bound - 1;
+        for (int bound = 1; bound < arr.length; bound++) {
+            int cur = bound -1 ;
             int tmp = arr[bound];
             for(; cur >= 0; cur--) {
-                if(arr[cur] > tmp) {
+                if(arr[cur] > tmp ) {
                     arr[cur+1] = arr[cur];
                 }else {
                     break;
