5b150e84d63a49fc73eb99fdb7611c1406fc3792
WithuL <1509586319@qq.com>
2020-08-09 16:53:41 +0800

    Create a string from a binary tree

--- src/Java_0809/Test.java
+++ src/Java_0809/Test.java
@@ -100,4 +100,19 @@ public class Test {
             return root;
         }
     }
+    class Solution4 {
+        public String tree2str(TreeNode t) {
+            if(t == null) {
+                return "";
+            }
+            if(t.left == null && t.right == null) {
+                return t.val + "";
+            }
+            if(t.right == null) {
+                return t.val + "(" + tree2str(t.left) + ")";
+            }
+            return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) +")";
+        }
+    }
+
 }
