91bd2bd6f838ea4f8ac97746d226aed3d581e57b
WithuL <1509586319@qq.com>
2020-08-22 15:38:47 +0800

    搜索旋转排序数组

---
+++ src/Java_0822/Test1.java
@@ -0,0 +1,34 @@
+package Java_0822;
+
+public class Test1 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            if(nums.length == 0) {
+                return -1;
+            }
+            int start = 0;
+            int end = nums.length - 1;
+            int mid;
+            while(start <= end) {
+                mid = (start + end) / 2;
+                if(nums[mid] == target) {
+                    return mid;
+                }
+                if(nums[start] <= nums[mid]) {
+                    if(nums[start] <= target && target < nums[mid]) {
+                        end = mid-1;
+                    }else {
+                        start = mid +1;
+                    }
+                }else {
+                    if(nums[mid] < target && target <= nums[end]) {
+                        start = mid + 1;
+                    }else {
+                        end = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
