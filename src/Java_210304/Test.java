8a1758c33f5dd6b30a51358e964d6aaf4f9d8626
withu <1509586319@qq.com>
2021-03-04 12:02:07 +0800

    sort

---
+++ src/Java_210304/Test.java
@@ -0,0 +1,29 @@
+package Java_210304;
+
+public class Test{
+    public static void main(String[] args){
+        Women women = new Women("yj");
+        System.out.println(women.name);
+        women.run();
+    }
+}
+class Man{
+    String name;
+    public Man(String name){
+        this.name = name;
+    }
+    public void run(){
+        System.out.println(name + "可以奔跑");
+    }
+}
+
+class Women extends Man{
+    public Women(String name) {
+        super(name);
+    }
+    @Override
+    public void run(){
+        System.out.println(name + "在化妆");
+    }
+}
+
